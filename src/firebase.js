7b3624215e5e543b49cbb8223cea81989e3b4e40
Sujit Mohanty <sujit.webdev@gmail.com>
2021-01-15 23:59:03 +0530

    initial setup with firebase and firestore

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementID: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+const frbs = firebase.initializeApp(firebaseConfig);
+export default frbs;
