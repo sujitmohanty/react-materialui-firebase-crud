53971af5923224c359601fddd048e71e07cf5ed0
Sujit Mohanty <sujit.webdev@gmail.com>
2021-02-02 23:06:11 +0530

    app v1

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase';
+import 'firebase/firestore';
 
-const firebaseConfig = {
+const firebaseApp = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -8,7 +9,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementID: process.env.REACT_APP_MEASUREMENT_ID,
-};
+});
 
-const frbs = firebase.initializeApp(firebaseConfig);
-export default frbs;
+const db = firebaseApp.firestore();
+export default db;
